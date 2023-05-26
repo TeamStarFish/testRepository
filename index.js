931b6d4ada0fdf641441044170c012953ab3c5c4
ThickCorgi <123416896+ThickCorgi@users.noreply.github.com>
2023-05-26 19:42:20 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+//
+
+const tes = []
+
+tes.push("chang")
+//tes.push("Jun")
+//tes.push("Cortland")
+//tes.push("Patrick")
+//tes.push("Don")
+
+
+function test(array){
+  
+  return array
+}
+
+test(tes)
